a2c1a6ff0b2a7862d2fa905177d6645ba233c2cf
anthonyknight023@gmail.com <anthonyknight023@gmail.com>
2018-03-15 22:50:20 -0400

    MVP

--- app/data/friends.js
+++ app/data/friends.js
@@ -12,7 +12,22 @@ var friend = [{
 		5,
 		4,
 		1
+	],
+	name:"JOHNNY",
+	photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+	scores:[
+		1,
+		1,
+		4,
+		4,
+		5,
+		1,
+		1,
+		5,
+		4,
+		4
 	]
+
 }];
 
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,6 +6,7 @@ var friends = require('../data/friends.js')
 function apiRoutes(app, path) {
 	
 	app.get("/api/friends", function(req, res) {
+
 	  	res.json(friends);
 	});
 
@@ -15,8 +16,34 @@ function apiRoutes(app, path) {
 		var questionnaire = req.body;
 		
 		console.log(questionnaire);
+
+		var quesScores = questionnaire.scores;
+		
+		var bestfriendmatchdifftotal = 100;
+		var bestFriend = {
+			name:'',
+			photo: ''
+		}
+
+		for (var i = 0; i < friends.length; i ++) {
+
+			var difftotal = 0;
+			for (var k = 0; k < friends[i].scores[k]; k ++)	{
+
+				difftotal += Math.abs(parseInt(quesScores[k]) - parseInt(friends[i].scores[k]));
+
+				if (difftotal < bestfriendmatchdifftotal) {
+					bestFriend.name = friends[i].name;
+					bestFriend.photo = friends[i].photo;
+					bestfriendmatchdifftotal = difftotal;
+				}
+
+			}
+
+		}
+
 		friends.push(questionnaire);
-		res.json(questionnaire);
+		res.json(bestFriend);
 
 	});
 
